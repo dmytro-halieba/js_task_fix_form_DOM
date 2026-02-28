c34eafb3054c80bdbfc10a93a6c9eecd42392330
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-03-01 00:55:38 +0200

    feat: label and placeholders to exicting inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const signUpForm = document.getElementsByTagName('form')[0];
+const signInForm = document.getElementsByTagName('form')[1];
+
+const signUpInputs = Array.from(signUpForm.getElementsByTagName('input'));
+const signInInputs = Array.from(signInForm.getElementsByTagName('input'));
+
+signUpInputs.forEach((input) => {
+  input.placeholder = input.name;
+
+  const label = document.createElement('label');
+
+  label.textContent = input.name;
+  label.className = 'field-label';
+
+  input.parentNode.insertBefore(label, input);
+});
+
+signInInputs.forEach((input) => {
+  input.placeholder = input.name;
+
+  const label = document.createElement('label');
+
+  label.textContent = input.name;
+  label.className = 'field-label';
+
+  input.parentNode.insertBefore(label, input);
+});
